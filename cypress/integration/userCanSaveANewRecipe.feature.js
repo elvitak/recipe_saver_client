6ad52674be173d78905f8c3f9e6308522508d64a
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-28 16:53:49 +0100

    add spec for adding a new recipe

--- cypress/integration/userCanSaveANewRecipe.feature.js
+++ cypress/integration/userCanSaveANewRecipe.feature.js
@@ -2,9 +2,21 @@
 describe("Visitingg the application, a user", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.get("#addNewRecipeTab").click();
   });
 
   it("is expected to see a header", () => {
-    cy.get("h1").should("contain", "Save your recipe here");
+    cy.get("[data-cy=addNewRecipe]").should("contain", "Save your recipe here");
+  });
+
+  it("is expected to enter data for a recipe", () => {
+    cy.get("[data-cy=form-title]").type("Cielaviņa");
+    cy.get("[data-cy=form-ingredient-amount]").type(2);
+    cy.get("[data-cy=form-ingredient-unit]").select("gb");
+    cy.get("[data-cy=form-ingredient-name]").type("olas");
+    cy.get("[data-cy=form-instructions]").type(
+      "Vispirms sāk ar bezē kārtu gatavošanu - olu baltumus (un te svarīgi atcerēties visu nozīmīgo par olbaltumu kulšanu - bļodai un putojamajām slotiņām jābūt super tīrām un sausām un baltumos nevar būt neviens kriksītis olu dzeltenumu, tad viss būs kārtībā) lej mikserbļodā, pievieno šķipsniņu sāls, tad sāk kulšanu, pēc kādas minūtes, kad olbaltumi kļuvuši gaisīgi, pa ēdamkarotei lēnām pievieno un iekuļ visu cukuru."
+    );
+    cy.get("[data-cy=form-add]").click();
   });
 });
