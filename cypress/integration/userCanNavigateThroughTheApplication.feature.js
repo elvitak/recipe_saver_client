91c012bb9c7e5c733f4f6acf302e6c4e4f588f1b
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-28 13:47:58 +0100

    fix semantic ui

--- cypress/integration/userCanNavigateThroughTheApplication.feature.js
+++ cypress/integration/userCanNavigateThroughTheApplication.feature.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe("User can navigate the app", () => {
   beforeEach(() => {
     cy.visit("/");
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FormToSaveRecipe from "./components/FormToSaveRecipe";
+import HeaderComponent from "./components/HeaderComponent";
 
 const App = () => {
   return (
     <React.Fragment>
-      <h1>Hello World</h1>
+      <HeaderComponent />
       <FormToSaveRecipe />
     </React.Fragment>
   );
