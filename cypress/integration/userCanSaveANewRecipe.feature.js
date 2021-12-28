ffc6583110a5b5d06dd21d0a42415e29db731fdf
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-28 17:52:41 +0100

    implement basic form for adding a new recipe

--- cypress/integration/userCanSaveANewRecipe.feature.js
+++ cypress/integration/userCanSaveANewRecipe.feature.js
@@ -12,10 +12,10 @@ describe("Visitingg the application, a user", () => {
   it("is expected to enter data for a recipe", () => {
     cy.get("[data-cy=form-title]").type("Cielaviņa");
     cy.get("[data-cy=form-ingredient-amount]").type(2);
-    cy.get("[data-cy=form-ingredient-unit]").select("gb");
+    cy.get("[data-cy=form-ingredient-unit]").type("g");
     cy.get("[data-cy=form-ingredient-name]").type("olas");
     cy.get("[data-cy=form-instructions]").type(
-      "Vispirms sāk ar bezē kārtu gatavošanu - olu baltumus (un te svarīgi atcerēties visu nozīmīgo par olbaltumu kulšanu - bļodai un putojamajām slotiņām jābūt super tīrām un sausām un baltumos nevar būt neviens kriksītis olu dzeltenumu, tad viss būs kārtībā) lej mikserbļodā, pievieno šķipsniņu sāls, tad sāk kulšanu, pēc kādas minūtes, kad olbaltumi kļuvuši gaisīgi, pa ēdamkarotei lēnām pievieno un iekuļ visu cukuru."
+      "Vispirms sāk ar bezē kārtu gatavošanu - olu baltumus lej mikserbļodā, pievieno šķipsniņu sāls, tad sāk kulšanu, pēc kādas minūtes, kad olbaltumi kļuvuši gaisīgi, pa ēdamkarotei lēnām pievieno un iekuļ visu cukuru."
     );
     cy.get("[data-cy=form-add]").click();
   });
--- src/components/FormToSaveRecipe.jsx
+++ src/components/FormToSaveRecipe.jsx
@@ -1,8 +1,41 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Form, Button } from "semantic-ui-react";
 
 const FormToSaveRecipe = () => {
-  return <Header data-cy="addNewRecipe">Save your recipe here</Header>;
+  return (
+    <>
+      <Header data-cy="addNewRecipe">Save your recipe here</Header>
+      <Form>
+        <Form.Field>
+          <label>Title</label>
+          <input data-cy="form-title" placeholder="Recipe title" />
+        </Form.Field>
+        <Form.Group widths="equal">
+          <Form.Input
+            data-cy="form-ingredient-amount"
+            label="Amount"
+            placeholder="2"
+          />
+          <Form.Input
+            data-cy="form-ingredient-unit"
+            label="Units"
+            placeholder="Units"
+          />
+          <Form.Input
+            data-cy="form-ingredient-name"
+            label="Ingredient"
+            placeholder="Ingredient"
+          />
+        </Form.Group>
+        <Form.TextArea
+          data-cy="form-instructions"
+          label="instructions"
+          placeholder="Take 2 egss and ..."
+        />
+      </Form>
+      <Button data-cy="form-add">Add Recipe</Button>
+    </>
+  );
 };
 
 export default FormToSaveRecipe;
