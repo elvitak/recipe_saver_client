009225a341a7d8eab1afd5375be756ae2ebf1508
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-09 14:17:20 +0100

    apply material ui for recipe collection

--- src/components/DisplayRecipeCollection.jsx
+++ src/components/DisplayRecipeCollection.jsx
@@ -1,14 +1,55 @@
 import React from "react";
+import {
+  Typography,
+  Container,
+  CardContent,
+  CardActions,
+  Button,
+  Grid,
+  Card,
+} from "@material-ui/core";
+import useStyles from "../styles/styles";
 
 const DisplayRecipeCollection = ({ recipeCollection }) => {
-  const recipeList = recipeCollection.map((recipe) => {
-    return <li key={recipe.id}>{recipe.title}</li>;
-  });
-
+  const classes = useStyles();
   return (
     <>
-      <h1 data-cy="collectionOfRecipes">Saved recipes</h1>
-      <ul data-cy="recipe-collection">{recipeList}</ul>
+      <main>
+        <div className={classes.container}>
+          <Typography
+            variant="h2"
+            align="left"
+            color="textPrimary"
+            gutterBottom
+            data-cy="collectionOfRecipes"
+          >
+            Saved recipes
+          </Typography>
+        </div>
+        <Container className={classes.cardGrid} disableGutters={true}>
+          <Grid container spacing={4}>
+            {recipeCollection.map((recipe) => (
+              <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      {recipe.title}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
     </>
   );
 };
