b1838f1bd2a56980944f0a13149eb9fb4b203a11
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-17 20:47:57 +0100

    rearrange userCanSaveANewRecipe spec, add message to fixture, delete unnecessary text from readme file

--- cypress/integration/userCanSaveANewRecipe.feature.js
+++ cypress/integration/userCanSaveANewRecipe.feature.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 describe("Visiting the application, a user", () => {
-  beforeEach(() => {
+  before(() => {
     cy.intercept("POST", "/api/recipes", {
       fixture: "create_response.json",
     });
     cy.visit("/");
     cy.get("#addNewRecipeTab").click();
+    cy.get("[data-cy=form-title]").type("Cielaviņa");
+    cy.get("[data-cy=form-ingredient-amount-0]").type(2);
+    cy.get("[data-cy=form-ingredient-unit-0]").type("g");
+    cy.get("[data-cy=form-ingredient-name-0]").type("olas");
+    cy.get("[data-cy=form-ingredient-amount-1]").type(150);
+    cy.get("[data-cy=form-ingredient-unit-1]").type("grami");
+    cy.get("[data-cy=form-ingredient-name-1]").type("miltu");
+    cy.get("[data-cy=form-instructions]").type(
+      "Vispirms sāk ar bezē kārtu gatavošanu - olu baltumus lej mikserbļodā, pievieno šķipsniņu sāls, tad sāk kulšanu."
+    );
   });
 
   it("is expected to see a header", () => {
@@ -15,21 +25,12 @@ describe("Visiting the application, a user", () => {
     );
   });
 
-  it("is expected to be able to enter data for a recipe", () => {
-    cy.get("[data-cy=form-title]").type("Cielaviņa");
-    cy.get("[data-cy=form-ingredient-amount-0]").type(2);
-    cy.get("[data-cy=form-ingredient-unit-0]").type("g");
-    cy.get("[data-cy=form-ingredient-name-0]").type("olas");
-    cy.get("[data-cy=form-ingredient-amount-1]").type(150);
-    cy.get("[data-cy=form-ingredient-unit-1]").type("ml");
-    cy.get("[data-cy=form-ingredient-name-1]").type("piens");
-    cy.get("[data-cy=form-ingredient-amount-2]").type(500);
-    cy.get("[data-cy=form-ingredient-unit-2]").type("gr");
-    cy.get("[data-cy=form-ingredient-name-2]").type("miltu");
-    cy.get("[data-cy=form-ingredient-input-line]").should("have.length", 4);
-    cy.get("[data-cy=form-instructions]").type(
-      "Vispirms sāk ar bezē kārtu gatavošanu - olu baltumus lej mikserbļodā, pievieno šķipsniņu sāls, tad sāk kulšanu, pēc kādas minūtes, kad olbaltumi kļuvuši gaisīgi, pa ēdamkarotei lēnām pievieno un iekuļ visu cukuru."
-    );
+  it("is expected to be able to enter new ingredient in an empty row", () => {
+    cy.get("[data-cy=form-ingredient-input-line]").should("have.length", 3);
+  });
+
+  it("is expected to see aproval message after saving new recipe", () => {
     cy.get("[data-cy=form-create-btn]").click();
+    cy.get("[data-cy=message]").should("contain", "Recipe was saved");
   });
 });
