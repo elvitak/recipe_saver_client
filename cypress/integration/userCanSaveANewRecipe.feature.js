9ac60a5df4178d6858b61a8a7d4e13bc840d1d58
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-02-05 18:44:12 +0100

    display success message when recipe has been created, add spec to check that message is visible and stub out call to api

--- cypress/integration/userCanSaveANewRecipe.feature.js
+++ cypress/integration/userCanSaveANewRecipe.feature.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef */
 describe("Visiting the application, a user", () => {
   before(() => {
-    cy.intercept("POST", "/api/recipes", {
-      fixture: "create_response.json",
-    });
     cy.intercept("GET", "/api/recipes", {
       fixture: "index_response.json",
     });
@@ -32,8 +29,17 @@ describe("Visiting the application, a user", () => {
     cy.get("[data-cy=form-ingredient-input-line]").should("have.length", 3);
   });
 
-  // it("is expected to see aproval message after saving new recipe", () => {
-  //   cy.get("[data-cy=form-create-btn]").click();
-  //   cy.get("[data-cy=message]").should("contain", "Recipe was saved");
-  // });
+  it("is expected to be able to press create", () => {
+    cy.intercept("POST", "/api/recipes", {
+      fixture: "create_response.json",
+    }).as("createRequest");
+    cy.get("[data-cy=form-create-btn]").click();
+  });
+
+  it("is expected to see aproval message after saving new recipe", () => {
+    cy.get("[data-cy=flash-message]").should(
+      "contain",
+      "Recipe was created successfully"
+    );
+  });
 });
