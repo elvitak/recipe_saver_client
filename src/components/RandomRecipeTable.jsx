e1d1268939309c8b15b5dc404faa90f64336e204
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-04-06 14:46:04 +0200

    add RandomRecipesTable component to divide code and make components shorter extract weekdays into separate file

---
+++ src/components/RandomRecipeTable.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableCell,
+  TableRow,
+  TableBody,
+  IconButton
+} from "@mui/material";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import Recipes from "../modules/recipes";
+import weekdays from "../modules/weekdays";
+
+const RandomRecipeTable = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const data = await Recipes.index(weekdays.length);
+      setRecipes(data.recipes);
+    };
+    fetchRecipes();
+  }, []);
+
+  const replaceRecipe = async (index) => {
+    const data = await Recipes.index(1);
+    recipes[index] = data.recipes[0];
+    setRecipes([...recipes]);
+  };
+
+  return (
+    <Table>
+      <TableBody>
+        {recipes.map((recipe, index) => (
+          <TableRow data-cy="random-recipes" key={recipe.id}>
+            <TableCell>{weekdays[index]}</TableCell>
+            <TableCell data-cy={`random-recipes-${index}`}>
+              {recipe.title}
+            </TableCell>
+            <IconButton onClick={() => replaceRecipe(index)}>
+              <ChangeCircleIcon
+                color="secondary"
+                data-cy={`change-random-recipe-${index}`}
+              />
+            </IconButton>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default RandomRecipeTable;
--- src/components/RandomRecipes.jsx
+++ src/components/RandomRecipes.jsx
@@ -1,65 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Recipes from "../modules/recipes";
-import {
-  Table,
-  TableCell,
-  TableRow,
-  TableBody,
-  IconButton
-} from "@mui/material";
-import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import React from "react";
 import Headline from "./Headline";
+import RandomRecipesTable from "./RandomRecipeTable";
 
 const RandomRecipes = () => {
-  const [recipes, setRecipes] = useState([]);
-  const weekdays = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-  ];
-
-  const fetchRecipes = async () => {
-    const randomSampleSize = 7;
-    const data = await Recipes.index(randomSampleSize);
-    setRecipes(data.recipes);
-  };
-
-  useEffect(() => {
-    fetchRecipes();
-  }, []);
-
-  const replaceRecipe = async (index) => {
-    const randomSampleSize = 1;
-    const data = await Recipes.index(randomSampleSize);
-    recipes[index] = data.recipes[0];
-    setRecipes([...recipes]);
-  };
-
   return (
     <>
       <Headline viewHeadline={"Your menu for a week"} />
-      <Table>
-        <TableBody>
-          {recipes.map((recipe, index) => (
-            <TableRow data-cy="random-recipes" key={recipe.id}>
-              <TableCell>{weekdays[index]}</TableCell>
-              <TableCell data-cy={`random-recipes-${index}`}>
-                {recipe.title}
-              </TableCell>
-              <IconButton onClick={() => replaceRecipe(index)}>
-                <ChangeCircleIcon
-                  color="secondary"
-                  data-cy={`change-random-recipe-${index}`}
-                />
-              </IconButton>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <RandomRecipesTable />
     </>
   );
 };
