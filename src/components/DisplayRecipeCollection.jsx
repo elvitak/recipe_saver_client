0ad7e2144ad4d81bee0af67ca05cc39fc9638919
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-04-21 11:01:38 +0200

    add condition for stages when recipes are still undefined, when there is zero recipes returned from api side and to dispaly recipes remove useState for message extract flashMessage into separate variable

--- src/components/DisplayRecipeCollection.jsx
+++ src/components/DisplayRecipeCollection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, CircularProgress } from "@mui/material";
 import Recipes from "../modules/recipes";
 import useStyles from "../styles/styles";
 import RecipeCard from "./RecipeCard";
@@ -7,19 +7,14 @@ import { useLocation } from "react-router-dom";
 import Headline from "./Headline";
 
 const DisplayRecipeCollection = () => {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState();
   const classes = useStyles();
   const { state } = useLocation();
-  const [message, setMessage] = useState();
   const [stateMessage, setStateMessage] = useState(state?.message);
 
   const fetchRecipes = async () => {
     const data = await Recipes.index();
-    if (data.recipes.length === 0) {
-      setMessage("You haven't saved any recipe yet");
-    } else {
-      setRecipes(data.recipes);
-    }
+    setRecipes(data.recipes);
   };
 
   useEffect(() => {
@@ -27,30 +22,45 @@ const DisplayRecipeCollection = () => {
   }, []);
 
   useEffect(() => {
-    setTimeout(() => setStateMessage(""), 3000);
+    setTimeout(() => setStateMessage(undefined), 3000);
   }, [state]);
 
-  const recipeCollection = recipes.map((recipe) => {
-    return (
-      <Grid item key={recipe.id} xs={12} sm={6} md={4}>
-        <RecipeCard recipe={recipe} />
-      </Grid>
+  let content;
+  if (recipes === undefined) {
+    content = <CircularProgress data-cy="loading-circle" />;
+  } else if (recipes.length === 0) {
+    content = (
+      <div data-cy="informational-message">
+        You haven't saved any recipe yet
+      </div>
     );
-  });
+  } else {
+    const recipeCollection = recipes.map((recipe) => {
+      return (
+        <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+          <RecipeCard recipe={recipe} />
+        </Grid>
+      );
+    });
+
+    content = (
+      <Container className={classes.cardGrid} disableGutters={true}>
+        <Grid data-cy="recipe-collection" container spacing={4}>
+          {recipeCollection}
+        </Grid>
+      </Container>
+    );
+  }
+
+  const flashMessage = stateMessage ? (
+    <div data-cy="flash-message">{stateMessage}</div>
+  ) : undefined;
 
   return (
     <>
       <Headline viewHeadline={"Saved recipes"} />
-      <div data-cy="flash-message">{stateMessage}</div>
-      {message ? (
-        <div data-cy="informational-message">{message}</div>
-      ) : (
-        <Container className={classes.cardGrid} disableGutters={true}>
-          <Grid data-cy="recipe-collection" container spacing={4}>
-            {recipeCollection}
-          </Grid>
-        </Container>
-      )}
+      {flashMessage}
+      {content}
     </>
   );
 };
