07d10bfa8afe983220b17956921f9c1c5cbd7e77
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-06-11 10:18:51 +0200

    change FormToSaveRecipe component to CreateAndUpdateRecipe in all places it is mentioned

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderComponent from "./components/HeaderComponent";
 import { Routes, Route } from "react-router";
-import FormToSaveRecipe from "./components/FormToSaveRecipe";
+import CreateAndUpdateRecipe from "./components/CreateAndUpdateRecipe";
 import DisplayRecipeCollection from "./components/DisplayRecipeCollection";
 import DisplaySingleRecipe from "./components/DisplaySingleRecipe";
 import RandomRecipes from "./components/RandomRecipes";
@@ -11,10 +11,10 @@ const App = () => {
     <>
       <HeaderComponent />
       <Routes>
-        <Route path="add-recipe" element={<FormToSaveRecipe />} />
+        <Route path="add-recipe" element={<CreateAndUpdateRecipe />} />
         <Route path="/" element={<DisplayRecipeCollection />} />
         <Route path="recipes/:id" element={<DisplaySingleRecipe />} />
-        <Route path="recipes/:id/edit" element={<FormToSaveRecipe />} />
+        <Route path="recipes/:id/edit" element={<CreateAndUpdateRecipe />} />
         <Route path="recipes/random-generated" element={<RandomRecipes />} />
       </Routes>
     </>
